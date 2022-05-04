214b815c6cd2eed0dbcf5119e7a9635002660f9d
RikoStuff <chachaerickjo@gmail.com>
2022-05-04 14:04:18 +0300

    ft - add userInterface logic and fix handlebars import

--- src/main/java/chachaup/Main.java
+++ src/main/java/chachaup/Main.java
@@ -1,7 +1,23 @@
 package chachaup;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        staticFileLocation("/public");
+        get("/",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/hero",(request,response) -> {
+            Map<String,Object> model = new HashMap<>();
+            return new ModelAndView(model,"add-hero.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
