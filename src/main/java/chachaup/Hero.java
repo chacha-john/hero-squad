824add7f0bff0f0b35bfab8ba3eb461ef9acd89f
RikoStuff <chachaerickjo@gmail.com>
2022-05-04 12:31:54 +0300

    ft - add models for the hero and squad objects

--- src/main/java/chachaup/Hero.java
+++ src/main/java/chachaup/Hero.java
@@ -1,2 +1,56 @@
-package chachaup;public class Hero {
+package chachaup;
+
+public class Hero {
+    private String heroName;
+    private int heroAge;
+    private String heroPower;
+    private String heroWeakness;
+    private Squad heroSquad;
+
+    public Hero(String heroName, int heroAge, String heroPower, String heroWeakness){
+        this.heroAge = heroAge;
+        this.heroName = heroName;
+        this.heroPower = heroPower;
+        this.heroWeakness = heroWeakness;
+    }
+
+    public Squad getHeroSquad() {
+        return heroSquad;
+    }
+
+    public void setHeroSquad(Squad heroSquad) {
+        this.heroSquad = heroSquad;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public int getHeroAge() {
+        return heroAge;
+    }
+
+    public void setHeroAge(int heroAge) {
+        this.heroAge = heroAge;
+    }
+
+    public String getHeroPower() {
+        return heroPower;
+    }
+
+    public void setHeroPower(String heroPower) {
+        this.heroPower = heroPower;
+    }
+
+    public String getHeroWeakness() {
+        return heroWeakness;
+    }
+
+    public void setHeroWeakness(String heroWeakness) {
+        this.heroWeakness = heroWeakness;
+    }
 }
--- src/main/java/chachaup/Squad.java
+++ src/main/java/chachaup/Squad.java
@@ -1,2 +1,37 @@
-package chachaup;public class Squad {
+package chachaup;
+
+public class Squad {
+    private int squadMaxSize;
+    private String squadName;
+    private String squadCause;
+
+    public Squad(String squadName, int squadMaxSize, String squadCause) {
+        this.squadCause = squadCause;
+        this.squadMaxSize = squadMaxSize;
+        this.squadName = squadName;
+    }
+
+    public int getSquadMaxSize() {
+        return squadMaxSize;
+    }
+
+    public void setSquadMaxSize(int squadMaxSize) {
+        this.squadMaxSize = squadMaxSize;
+    }
+
+    public String getSquadName() {
+        return squadName;
+    }
+
+    public void setSquadName(String squadName) {
+        this.squadName = squadName;
+    }
+
+    public String getSquadCause() {
+        return squadCause;
+    }
+
+    public void setSquadCause(String squadCause) {
+        this.squadCause = squadCause;
+    }
 }
