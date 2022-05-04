838583b46814ef8d88c9aed6be9fac7417ef49e1
RikoStuff <chachaerickjo@gmail.com>
2022-05-04 12:32:52 +0300

    ft - add tests for instantiation of the hero and squad objects

--- src/test/java/chachaup/HeroTest.java
+++ src/test/java/chachaup/HeroTest.java
@@ -2,8 +2,10 @@ package chachaup;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class HeroTest {
 
     @BeforeEach
@@ -12,5 +14,11 @@ class HeroTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void heroInstantiatesCorrectly() {
+        Hero hero = new Hero("chacha",24,"invisibility","believes in people");
+        assertTrue(hero instanceof Hero);
     }
 }
--- src/test/java/chachaup/SquadTest.java
+++ src/test/java/chachaup/SquadTest.java
@@ -2,6 +2,7 @@ package chachaup;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +14,11 @@ class SquadTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void squadInstantiatesCorrectly_true() {
+        Squad squad = new Squad("chachaUp",12,"poverty");
+        assertTrue(squad instanceof Squad);
     }
 }
