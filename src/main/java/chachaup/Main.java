4359a7e73f0c9b2e03311a085e2c3693aa5d3636
RikoStuff <chachaerickjo@gmail.com>
2022-05-04 16:51:09 +0300

    ft - add functionality for post requests and to display all Heroes

--- src/main/java/chachaup/Main.java
+++ src/main/java/chachaup/Main.java
@@ -3,6 +3,7 @@ package chachaup;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,11 +14,23 @@ public class Main {
         staticFileLocation("/public");
         get("/",(request, response) -> {
             Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> heroes = Hero.getAll();
+            model.put("heroes",heroes);
             return new ModelAndView(model,"index.hbs");
         }, new HandlebarsTemplateEngine());
         get("/hero",(request,response) -> {
             Map<String,Object> model = new HashMap<>();
             return new ModelAndView(model,"add-hero.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/hero/new",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String heroName = request.queryParams("heroName");
+            String heroPower = request.queryParams("heroPower");
+            String heroWeakness = request.queryParams("heroWeakness");
+            int heroAge = Integer.parseInt(request.queryParams("heroAge"));
+
+            Hero hero = new Hero(heroName,heroAge,heroPower,heroWeakness);
+            return new ModelAndView(model,"heroes.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
